1adbf9bf8ac95a549f0b89167c64643fe4de3d87
rmandell06 <robertmandell237@gmail.com>
2026-07-21 12:30:27 -0500

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        std::queue<char> q;
+        std::stack<char> st;
+
+        for(char letter: s){
+            if((letter >= 97 && letter <=122) || (letter >= 65 && letter <=90) || (letter >= 48 && letter <=57)){
+                q.push(letter);
+                st.push(letter);
+            }
+
+        }
+
+        for( int i = 0 ; i < st.size() ; i++){
+            std::cout<<q.front()<<'\t'<<st.top()<<std::endl;
+            if(std::tolower(q.front()) != std::tolower(st.top())) return false;
+            q.pop();
+            st.pop();
+        }
+        return true;
+    }
+};
