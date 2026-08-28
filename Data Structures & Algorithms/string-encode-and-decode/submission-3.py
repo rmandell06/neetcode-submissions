87667dc85446573f08b4c848ba11f33bc082641c
rmandell06 <robertmandell237@gmail.com>
2026-08-27 22:57:13 -0500

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_str = ""
+        for string in strs:
+            encoded_str += (string +'\0')
+
+        return encoded_str
+    def decode(self, s: str) -> List[str]:
+        return s.split('\0')[:-1]
